ec8d779f02c1dabff8be5c844bece53974fc75a8
Shaurya Dhingra <shauryadhingra1@gmail.com>
2025-08-12 11:06:36 +0530

    Initial commit for week-1-todo-list-cli

---
+++ projects/week-1-todo-list-cli/todo-list.sh
@@ -0,0 +1,76 @@
+while true; do
+	read -p "Choose Command: (1- Add Task, 2- List Tasks, 3- Mark Task as Done, 4- List Tasks Done, 5- Delete Task, 6- Exit) " cmd
+
+	case "$cmd" in
+	  1)
+	    read -p "Enter Task: " task
+	    echo "$task:Pending" >> tasklist.txt
+	    ;;
+	  2)
+	    if [ -f tasklist.txt ]; then
+		    grep ":Pending" tasklist.txt | nl
+		   
+	    else
+	      echo "No tasks found."
+	    fi
+	    ;;
+	  3)	
+		  echo "Current Tasks: "
+		  grep ":Pending" tasklist.txt | nl
+		  read -p "Enter task number to mark as done: " taskno
+		  if ! [[ "$taskno" =~ ^[0-9]+$ ]]; then
+		    echo "Invalid input. Must be a number."
+		    exit 1
+		  else
+			  task=$(sed -n "${taskno}p" tasklist.txt)
+			  echo "$task"
+			  if [ -f tasklist.txt ]; then
+				   
+				  sed -i "${taskno}d" tasklist.txt
+				  echo "$task:Done" >> taskdone.txt
+				  echo "Marked Task "$task" as Done."
+				  echo "Tasks Done: "
+				  grep "$task:Done" taskdone.txt | nl
+			  else
+				  echo "No tasks found"
+			  fi
+		  fi
+	    ;;
+    4)	
+	    if [ -f taskdone.txt ]; then                                
+                                  echo "Tasks Done: "
+                                  nl taskdone.txt
+                          else
+                                  echo "No tasks found"
+                          fi
+	 ;;
+
+
+
+	  5)  
+		
+		  echo "Current Tasks: "
+		  grep ":Pending" tasklist.txt | nl
+		  read -p "Enter task number to delete: " taskno
+		  if ! [[ "$taskno" =~ ^[0-9]+$ ]]; then
+		    echo "Invalid input. Must be a number."
+		    exit 1
+		  else
+			  task=$(sed -n "${taskno}p" tasklist.txt)
+			  if [ -s tasklist.txt ]; then	  
+				  sed -i "${taskno}d" tasklist.txt
+				  echo "Task "$task" Deleted."
+			  else
+				  echo "No tasks found"
+			  fi
+		  fi
+	    ;;
+
+    6)		echo "Exiting..."
+			break
+		;;
+	    *)
+		    echo "Invalid option."
+		    ;;
+		esac
+done
