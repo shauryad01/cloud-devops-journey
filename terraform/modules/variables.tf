c299790e59f82742a09d176af6a7fcef3421593c
Shaurya Dhingra <shauryadhingra1@gmail.com>
2025-11-21 17:22:41 +0530

    Add variables.tf to use variables within ec2.tf

---
+++ terraform/modules/variables.tf
@@ -0,0 +1,14 @@
+variable "ec2_instance_type" {
+  default = "t2.micro"
+  type    = string
+}
+
+variable "ec2_root_storage_size" {
+  default = 8
+  type    = number
+}
+
+variable "ec2_ami_id" {
+  default = "ami-0ecb62995f68bb549"
+  type    = string
+}
