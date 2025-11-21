89a3db08f58caadef2efeb935f6fa5494c8e1cdd
Shaurya Dhingra <shauryadhingra1@gmail.com>
2025-11-21 17:53:29 +0530

    Added outputs.tf to get output block for public and private IP and DNS

---
+++ terraform/modules/outputs.tf
@@ -0,0 +1,12 @@
+output "ec2_public_ip" {
+  value = aws_instance.ec2instance.public_ip
+}
+output "ec2_public_dns" {
+  value = aws_instance.ec2instance.public_dns
+}
+output "ec2_private_ip" {
+  value = aws_instance.ec2instance.private_ip
+}
+output "ec2_private_dns" {
+  value = aws_instance.ec2instance.private_dns
+}
